92b6639c4098c9f32d2bee482caa37c9371b9964
EzequielCalcanat <79470817+EzequielCalcanat@users.noreply.github.com>
2022-10-24 11:12:05 -0500

    pantallas juegos

--- src/app/games/sudoku/sudoku.component.ts
+++ src/app/games/sudoku/sudoku.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sudoku.component.css']
 })
 export class SudokuComponent implements OnInit {
-
+  public juegofacil: boolean = false;
+  public juegointermedio: boolean = false;
+  public juegoavanzado: boolean = false;
+  public pantallamenu: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarPantalla(tipo: string){
+    this.juegofacil = false;
+    this.juegointermedio = false;
+    this.juegoavanzado = false;
+    this.pantallamenu = false;
+    if(tipo == "menu"){
+      this.pantallamenu = true;
+    }else if(tipo == "facil"){
+      this.juegofacil = true;
+    }else if(tipo == "intermedio"){
+      this.juegointermedio = true;
+    }else if(tipo == "avanzado"){
+      this.juegoavanzado = true;
+    }
   }
 
 }
